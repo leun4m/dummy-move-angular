9c4810239afa8f585d04b2fd7956c560e78992c2
Manuel Krebs <manuelkrebs@online.de>
2018-11-07 20:23:50 +0100

    Move Component inside one component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewContainerRef, AfterViewInit, ComponentFactoryResolver } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, AfterViewInit, ComponentFactoryResolver, ComponentRef } from '@angular/core';
 import { ChildAComponent } from './child-a/child-a.component';
 
 @Component({
@@ -8,11 +8,22 @@ import { ChildAComponent } from './child-a/child-a.component';
 })
 export class AppComponent implements AfterViewInit{
   @ViewChild('containerA', {read: ViewContainerRef}) containerA: ViewContainerRef;
+  @ViewChild('containerB', {read: ViewContainerRef}) containerB: ViewContainerRef;
+
+  private childContainerRef: ComponentRef<ChildAComponent>;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
   ngAfterViewInit() {
     const factoryA = this.componentFactoryResolver.resolveComponentFactory(ChildAComponent);
-    this.containerA.createComponent(factoryA);
+    this.childContainerRef = this.containerA.createComponent(factoryA);
+    this.childContainerRef.changeDetectorRef.detectChanges();
+  }
+
+  moveDown() {
+    const index = this.containerA.indexOf(this.childContainerRef.hostView);
+    this.containerA.detach(index);
+    this.containerB.insert(this.childContainerRef.hostView);
+    this.childContainerRef.changeDetectorRef.detectChanges();
   }
 }
